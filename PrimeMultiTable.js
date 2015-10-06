bc9d2505b4789ed22fc975ccf6c53893823ac95e
ntoscano <Nicolas.Toscano@gmail.com>
2015-10-05 17:51:22 -0700

    notable changes: 	File now runs from command line 	Recursive function now replaced with while loop to prevent unnessesary callstack creation 	"current" variable inside "_primeArray" now optimized to skip over unnessesary numbers 	Function names changed to reflect privacy 	"table" vaiable population in "_tableCreator" changed to stay consistent with "_range" array population syntax

--- PrimeMultiTable.js
+++ PrimeMultiTable.js
@@ -1,4 +1,6 @@
 // PrimeMultiTable.js
+
+var Table = require('cli-table');
 /*
 The first two functions, range and primeArray, are helper functions.
 tableCreator is the function that created the matrix.
@@ -7,7 +9,7 @@ tableCreator is the function that created the matrix.
 
 /*
 Time complexity is defined by the most expensive opperation in a function. 
-  The most expensive opperations here are the for-loop inside the recursive function call 'primeIdentifyer'
+  The most expensive opperations here are the for-loop inside the while loop in "_primeArray"
   and the nested for-loop in 'tableCreator'.
 
   This means the time complexity is O(n^2).
@@ -21,52 +23,69 @@ Space complexity is defined by how many objects in memory get instantiated in re
   given that the requirements of the problem were to create an n * n matrix.
 */
 
-var range = function (n) {
+var _range = function (n) {
   var result = [];
-  for (var i = 0; i < n; i++){
+  for (var i = 0; i <= n; i++){
     result.push(i);
   }
   return result;
 };
 
-var primeArray = function (n) {
+var _primeArray = function (n) {
   var current = 2; 
-  var primes = range(n + 1);
+  var primes = _range(n);
   // While we haven't found all the primes.
-  var primeIdentifyer = function(){
-    //Once we reach the n of the desired recursion cycle, filter nonprimes from final array
-    if(current >= n){
-      return primes.slice(2).filter(function (val) {
-        return val;
-      });    
-    }else{
-      for (var i = current + current; i <= n; i += current) {
-        // null means not prime, a number means prime
-        primes[i] = null;
-      }  
+  while (current < n) {
+  //Once we reach the n of the desired recursion cycle, filter nonprimes from final array
+    for (var i = current + current; i <= n; i += current) {
+      // null means not prime, a number means prime
+      primes[i] = null;
+    }  
+    do {
       // Advance current at least once
       current += 1;
-      // Then continue to advance it until we hit a prime number or we are out of range.
-      return primeIdentifyer(); 
-    }
+      // Then continue to advance it until we hit a prime number or we are out of
+      // range.
+    } while (!primes[current] && current <= n);
+    // Then continue to advance it until we hit a prime number or we are out of range.
   };
-  return primeIdentifyer();
+  return primes.slice(2).filter(function (val) {
+    return val;
+  });;
 };
 
-var tableCreater = function(n){
+var _tableCreater = function(n){
   if(typeof n !== 'number' || n < 2 || n !== Math.floor(n)) return "tableCreater only takes positive integers greater than 1";
   var table = [];
   //Populate first row
-  table.push(primeArray(n));
-  table[0].unshift(null);
+  table.push(['null'].concat(_primeArray(n)));
+  // table[0].unshift(null);
   //Populate first column
   for(var i = 1; i < table[0].length; i++){
-    table[i] = [table[0][i]];
+    table.push([table[0][i]]);
     //Populate the rest of the row
     for(var j = 1; j < table[0].length; j++){
       table[i][j] = table[i][0] * table[0][j];
     }
   }
-  //console.log(table);
   return table;
 };
+
+var matrix = _tableCreater(parseInt(process.argv[2]));
+
+var table = new Table({
+    head: matrix[0]
+});
+
+for(var i = 1; i < matrix[0].length; i++){
+  table.push(matrix[i]);
+}
+
+
+console.log(table.toString());
+
+
+
+
+
+
